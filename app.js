8ba2eaa0782f6b77ce99cfeeaff396eca1470c7b
Ben Mottershead <mottershead.benj@gmail.com>
2021-02-23 15:50:41 +0000

    Return of correct data

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-const conn = require("./conn/db.conn");
+const config = require("./conn/db.conn");
+var sql = require("mssql");
 
 const express = require("express");
 const app = express();
 const port = 5000;
 
 app.get("/scpList", (req, res) => {
-  res.send(conn("SELECT * FROM SCP"));
+  runQuery("SELECT * FROM SCP", res);
 });
 
 app.listen(port, () => console.log("example application"));
+
+runQuery = (query, res) => {
+  sql.connect(config, function (err) {
+    if (err) console.log(err);
+
+    var req = new sql.Request();
+
+    req.query(query, function (err, data) {
+      if (err) console.log(err);
+      console.log(data.recordset);
+      res.send(data.recordset);
+    });
+  });
+};
+
+module.exports = runQuery;
--- conn/db.conn.js
+++ conn/db.conn.js
@@ -1,5 +1,3 @@
-var sql = require("mssql");
-
 var config = {
   user: "sa",
   password: "Pa$$word2020BM",
@@ -7,18 +5,4 @@ var config = {
   database: "fyp_scm",
 };
 
-var runQuery = (query, res) => {
-  sql.connect(config, function (err) {
-    if (err) console.log(err);
-
-    var req = new sql.Request();
-
-    req.query(query, function (err, recordset) {
-      if (err) console.log(err);
-      console.log(recordset.recordset);
-      return recordset.recordset;
-    });
-  });
-};
-
-module.exports = runQuery;
+module.exports = config;
